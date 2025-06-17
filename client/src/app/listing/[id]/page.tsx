e20657e17b0a7231633b1738101080287ea16219
erickdeoalamsyah <erickdeo29@gmail.com>
2025-06-17 19:50:13 +0700

    fix error param on listing [id]

--- client/src/app/listing/[id]/page.tsx
+++ client/src/app/listing/[id]/page.tsx
@@ -2,7 +2,8 @@ import { Suspense } from "react";
 import ProductDetailsSkeleton from "./productSkeleton";
 import ProductDetailsContent from "./productDetails";
 
-function ProductDetailsPage({ params }: { params: { id: string } }) {
+async function ProductDetailsPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   return (
     <Suspense fallback={<ProductDetailsSkeleton />}>
       <ProductDetailsContent id={params.id} />
